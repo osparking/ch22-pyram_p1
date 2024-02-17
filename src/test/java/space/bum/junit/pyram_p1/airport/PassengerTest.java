19bd832a4ec035432e859cade700585617ccd4ee
osparking <jbpark03@gmail.com>
2024-02-17 12:55:36 +0900

    정보 변경 시험 메소드 2 건

--- src/test/java/space/bum/junit/pyram_p1/airport/PassengerTest.java
+++ src/test/java/space/bum/junit/pyram_p1/airport/PassengerTest.java
@@ -13,6 +13,21 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class PassengerTest {
+  @Test
+  @DisplayName("내국인 등록 번호 변경 성공 시험")
+  public void testSetValidRrn() {
+    Passenger passenger = new Passenger("831010-4234567", "최은서", "KR");
+    passenger.setIdentifier("821010-2234567");
+    assertEquals("821010-2234567", passenger.getIdentifier());
+  }
+
+  @Test
+  @DisplayName("외국인 등록 번호 변경 성공 시험")
+  public void testSetValidNonUsIdentifier() {
+    Passenger passenger = new Passenger("840108-5234567", "김정은", "KP");
+    passenger.setIdentifier("840108-7234567");
+    assertEquals("840108-7234567", passenger.getIdentifier());
+  }
 
   @Test
   @DisplayName("외국인 등록번호 오류로 인한 예외 2 건 발생 시험")
